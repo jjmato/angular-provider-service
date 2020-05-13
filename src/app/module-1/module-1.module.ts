b04f8a82964c884ddd4ea11948b89fe368de4aa5
jjmato <jjmatotorres@gmail.com>
2020-05-13 10:20:41 +0200

    boat

--- src/app/module-1/module-1.module.ts
+++ src/app/module-1/module-1.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CarComponent } from './car/car.component';
+import { CarService } from './car.service';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [CarComponent]
+  declarations: [CarComponent],
+  providers: [CarService]
 })
 export class Module1Module { }
---
+++ src/app/module-2/module-2.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BoatComponent } from './boat/boat.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [BoatComponent]
+})
+export class Module2Module { }
