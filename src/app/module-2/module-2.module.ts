6af4eb168208869d6ec1602d4b4c8467a562044f
jjmato <jjmatotorres@gmail.com>
2020-05-13 10:20:57 +0200

    core

--- src/app/module-2/module-2.module.ts
+++ src/app/module-2/module-2.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BoatComponent } from './boat/boat.component';
+import { BoatService } from './boat.service';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [BoatComponent]
+  declarations: [BoatComponent],
+  providers: [BoatService]
 })
 export class Module2Module { }
