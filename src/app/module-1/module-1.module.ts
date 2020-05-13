192f6abe562047a3ceac4b46d89369d39d10b49c
jjmato <jjmatotorres@gmail.com>
2020-05-13 10:19:54 +0200

    car

--- src/app/module-1/module-1.module.ts
+++ src/app/module-1/module-1.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CarComponent } from './car/car.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [CarComponent]
 })
 export class Module1Module { }
