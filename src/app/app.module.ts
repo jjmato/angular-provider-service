e1219f86912d118e71124dcca1b669394dbc4549
jjmato <jjmatotorres@gmail.com>
2020-05-13 10:21:37 +0200

    import modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { CoreModule } from "./core/core.module";
+import { Module1Module } from "./module-1/module-1.module";
+import { Module2Module } from "./module-2/module-2.module";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    CoreModule,
+    Module1Module,
+    Module2Module
+  ],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { VehicleService } from './vehicle.service';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [],
+  providers: [VehicleService]
+})
+export class CoreModule { }
